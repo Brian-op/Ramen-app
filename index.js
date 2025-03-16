92a9afb3a43a01b461d4a75c7ea27151faaffeea
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-16 15:15:23 +0300

    ramen form HTML

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+ ];
+
+ let h1 = document.getElementById("h1")
+    h1.innerText = "Ramen Menu";
+
+let h2 = document.getElementById("h2")
+    h2.innerText = "Ramen Details"
+
+let h2 = document.getElementById("formHeader")
+    h2.innerText = "Insert New Ramen"
+
+ 
+
+  
+
+
